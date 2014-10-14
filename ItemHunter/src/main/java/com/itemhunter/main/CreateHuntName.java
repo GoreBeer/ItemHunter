65b12c7942b1240b1b75041ca6b6b968a873cb5d
KyleCombeer <keyhole150@gmail.com>
2014-10-14 21:29:33 +1100

    Finished CreateHuntNameDialog now, it suggests a name based on today's date and uses it if the user doesn't enter any data

--- ItemHunter/src/main/java/com/itemhunter/main/CreateHuntName.java
+++ ItemHunter/src/main/java/com/itemhunter/main/CreateHuntName.java
@@ -3,63 +3,65 @@ package com.itemhunter.main;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
 import com.itemhunter.sqlite.AppConstants;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * Created by Kyle on 18/09/2014.
  */
 public class CreateHuntName extends Activity {
+    protected String suggestedName = "hunt_";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_hunt_name);
 
-        //TODO - Create suggested title based on datetime
+        //Construct a datetime object to get today's date in a certain format
+        DateFormat df = new SimpleDateFormat("MMddyyyyHHmmss");
+        Date today = Calendar.getInstance().getTime();
+        String strDate = df.format(today);
+        suggestedName+=strDate;
+
+        //Set the hint to be today's date
+        EditText titleText = (EditText) findViewById(R.id.hunt_name);
+        titleText.setHint(suggestedName);
+
+        Log.v(AppConstants.TAG, "CreateHuntName Dialog instantiated successfully");
     }
 
-    /*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    */
-
-    //Cancel dialog
+    //Called by the Cancel button
     public void cancelDialog(View view){
         Intent intent = new Intent();
         setResult(RESULT_CANCELED, intent);
+
+        Log.v(AppConstants.TAG, "CreateHuntName Dialog cancelled");
         finish();
     }
 
-    //Create button new hunt dialog
+    //Called by the Create button
     public void createHunt(View view){
-        Intent intent = new Intent();
         EditText titleText = (EditText) findViewById(R.id.hunt_name);
-        Log.i(AppConstants.TAG, "Title text is: " + titleText.getText().toString());
+
+        //If no name has been chosen, we set it to the placeholder text
+        if(titleText.getText().toString().equalsIgnoreCase("")){
+            titleText.setText(suggestedName);
+        }
+
+        //Create a new intent, set the title and return back to new_hunt activity
+        Intent intent = new Intent();
         intent.putExtra(AppConstants.TITLE, titleText.getText().toString());
         setResult(RESULT_OK, intent);
+
+        Log.v(AppConstants.TAG, "CreateHuntName Dialog finished successfully");
         finish();
     }
 }
